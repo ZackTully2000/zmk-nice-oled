9d0c7374aae00e1ec75006fdd65c63c74f68e43f
ZackTully <tully@mines.edu>
2025-04-27 09:26:21 -0600

    Make some pixels black

---
+++ zack_stuff/change_diamond_animation.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+
+
+
+
+crystal_path = Path(r"C:\Users\zackt\Documents\keyboards\zmk-nice-oled\boards\shields\nice_oled\assets\crystal.c")
+
+with open(crystal_path, "w", encoding="utf-8") as f:
+    lines = f.readlines()
+
+[]    
